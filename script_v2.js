e28aa64513537e6c37f1da3e8dcb41968b9c1d43
TheAmazingBeat <38993027+TheAmazingBeat@users.noreply.github.com>
2019-04-04 11:34:13 -0500

    Minor Changes
    - Cleaned out commented arrays
    - added input of due date of homework
    - added storeHw(); This function has the part from prioritize() function

--- script_v2.js
+++ script_v2.js
@@ -1,19 +1,9 @@
 /*These are variables that are needed to be accessed in different functions*/
 var classes = [];
 classForm = document.querySelector(".class-form"); //the input for the name of the classes
-
-//classDifficulty = [];
-
-classValue = [];
-homework = [];
 homeworks = [];
-homeworkDifficulty = [];
-homeworkType = [];
 hwCounter = [];
 instructions = document.querySelector(".instructions");
-
-//nameOfClasses = []; //name of the classes (value of the classesInput)
-
 numOfClasses = document.querySelector(".number-of-classes");
 numClassForm = document.querySelector(".num-class-form"); //the input for the number of classes
 priorList = [];
@@ -33,7 +23,7 @@ function start() {
 }
 
 
-/*creates the "next"button*/
+/*creates the "next" button*/
 function createNextButton(whichButton) {
   var nextButton = document.createElement("button");
   var nextButtonText = document.createTextNode("Next");
@@ -68,16 +58,12 @@ function createClassForm() {
 
   createNextButton(0);
 
-  /*stores the name of the classes into nameOfClasses array*/
+  /*stores the name of the classes into classes.name*/
   document.querySelectorAll(".next-button")[0].onclick = function() {
     for (i = 0; i < (numOfClasses.value); i++) {
       classes[i] = new Object();
       classes[i].name = document.querySelectorAll(".class-form-input")[i].value;
-      //nameOfClasses[i] = document.querySelectorAll(".class-form-input")[i].value;
     }
-
-    //console.log("nameOfClasses: " + nameOfClasses);
-
     console.log(classes);
     difficulty();
   };
@@ -88,12 +74,8 @@ function difficulty() {
   document.querySelector(".difficulty").classList.toggle("invisible");
   instructions.innerText = "Rank your classes' difficulty from least to greatest:";
 
-
   for (i = 0; i < (numOfClasses.value); i++) {
     /*rewrites the classes in a list*/
-
-    //var className = nameOfClasses[i];
-    
     var classItemList = document.createElement("ul");
     var classItemListName = document.createTextNode(classes[i].name);
     classItemList.appendChild(classItemListName);
@@ -104,6 +86,7 @@ function difficulty() {
     var difficultySelect = document.createElement("select");
     difficultySelect.classList.add("select-difficulty");
     var valueCounter = 1;
+
     /*creates the options in dropdown menu*/
     for (x = 0; x < (numOfClasses.value); x++) {
       var difficultyOption = document.createElement("option");
@@ -113,7 +96,9 @@ function difficulty() {
       difficultySelect.appendChild(difficultyOption);
       valueCounter++;
     }
+
     difficultySelect.options[i].selected = true;
+
     /*makes the dropdown menu beside each item in the list*/
     var classItems = document.querySelectorAll("ul.class-item");
     for (y = 0; y < classItems.length; y++) {
@@ -127,14 +112,9 @@ function difficulty() {
   document.querySelectorAll(".next-button")[1].onclick = function() {
     for (z = 0; z < numOfClasses.value; z++) {
       classes[z].difficulty = document.querySelectorAll(".select-difficulty")[z].value;
-
-      //classDifficulty[z] = document.querySelectorAll(".select-difficulty")[z].value;
-
     }
-
-    //console.log("classDifficulty: " + classDifficulty);
-    
     console.log(classes);
+
     document.querySelector(".difficulty").classList.toggle("invisible");
     document.querySelector(".hw-manager").classList.toggle("invisible");
     instructions.innerText = "Now, add your homework for any class";
@@ -142,15 +122,15 @@ function difficulty() {
 }
 
 
-/*Adds a text input for homework's name, a dropdown for the type of homework, and a dropdown for which class its for*/
+/*Adds a text input, dropdown menus for type, class, and due date*/
 function addHomework() {
   var homeworkItem = document.createElement("ul");
 
-  var homeworkInput = document.createElement("input");
-    homeworkInput.type = "text";
-    homeworkInput.classList.add("homework-input");
+  var hwInput = document.createElement("input");
+    hwInput.type = "text";
+    hwInput.classList.add("homework-input");
 
-  /*creates options for the type of homework dropdown*/
+  /*creates a dropdown menu for homework type, then the options in it*/
   var hwType = document.createElement("select");
     hwType.classList.add("hw-type-dropdown");
   var homeworkMinor = document.createElement("option");
@@ -162,7 +142,7 @@ function addHomework() {
     hwType.appendChild(homeworkMinor);
     hwType.appendChild(homeworkMajor);
 
-  /*creates the options in the class dropdown*/
+  /*creates a dropdown menu for homework type, the options in it*/
   var hwClass = document.createElement("select");
   hwClass.classList.add("hw-class");
   for (i = 0; i < numOfClasses.value; i++) {
@@ -173,55 +153,63 @@ function addHomework() {
       hwClass.appendChild(classOption);
   }
 
-  homeworkItem.appendChild(homeworkInput);
+  /*creates a date dropdown menu*/
+  var hwDate = document.createElement("input");
+  hwDate.classList.add("hw-date");
+  hwDate.setAttribute("type", "date");
+
+  homeworkItem.appendChild(hwInput);
   homeworkItem.appendChild(hwType);
   homeworkItem.appendChild(hwClass);
+  homeworkItem.appendChild(hwDate);
 
   hwCounter.push(document.querySelector(".hw-list").appendChild(homeworkItem));
 }
 
-/*organizes the homework from highest difficulty and lowest difficulty*/
-function prioritize(numOfHw) {
+
+/*stores all values from input elements created in addHomework()*/
+function storeHW(numOfHw){
   for (i = 0; i < numOfHw; i++) {
     homeworks[i] = new Object();
     homeworks[i].name = document.querySelectorAll(".homework-input")[i].value;
     homeworks[i].type = document.querySelectorAll(".hw-type-dropdown")[i].value;
-    homeworks[i].class = document.querySelectorAll(".hw-class")[i].value;
-
-    // homework[i] = document.querySelectorAll(".homework-input")[i].value;
-    // homeworkType[i] = document.querySelectorAll(".hw-type-dropdown")[i].value;
-    // homeworkDifficulty[i] = document.querySelectorAll(".hw-class")[i].value;
+    homeworks[i].classDiff = document.querySelectorAll(".hw-class")[i].value;
+    homeworks[i].dueDate = document.querySelectorAll(".hw-date")[i].value;
   }
   console.log(homeworks);
+}
 
-  // console.log(homeworkType);
-  // console.log(homeworkDifficulty);
+
+/*organizes the homework from highest difficulty and lowest difficulty*/
+function prioritize(numOfHw) {
+  storeHW(numOfHw);
 
   for (i = 0; i < numOfHw; i++) {
     if (homeworks[i].type === 'Major') { //if it is a major homework, it will be the very first
       priorList.unshift(homeworks[i].name);
-    } else if (homeworks[i].class >= (Math.max(...homeworks[i].difficulty))) {
+    } else if (homeworks[i].classDiff >= (Math.max(...homeworks[i].difficulty))) {
         if(priorList.indexOf(priorList[i-1]) == 0 && homeworks[i-1].type != 'Major'){ //replaces the one in front, if and only the front is not a major homework
           priorList.splice(0, 0, homeworks[i].name);
         } else {
           priorList.splice(1, 0, homeworks[i].name);
         }
-    } else if (homeworks[i].class >= 8) { //8 and 9 difficulty are near the first
+    } else if (homeworks[i].classDiff >= 8) { //8 and 9 difficulty are near the first
       priorList.splice(priorList.length - 4, 0, homeworks[i].name);
-    } else if (homeworks[i].class >= 6) { //6 and 7 difficulty are near the first
+    } else if (homeworks[i].classDiff >= 6) { //6 and 7 difficulty are near the first
       priorList.splice(priorList.length - 3, 0, homeworks[i].name);
-    } else if (homeworks[i].class >= 4) { //4 and 5 difficulty are in the middle
+    } else if (homeworks[i].classDiff >= 4) { //4 and 5 difficulty are in the middle
       priorList.splice(priorList.length - 2, 0, homeworks[i].name);
-    } else if (homeworks[i].class >= 2) { //2 and 3 difficulty are near the last homework
+    } else if (homeworks[i].classDiff >= 2) { //2 and 3 difficulty are near the last homework
       priorList.splice(priorList.length - 1, 0, homeworks[i].name);
-    } else if (homeworks[i].class <= (Math.min(...homeworkDifficulty))) { //if it is the less difficult, homework will be pushed to the last
+    } else if (homeworks[i].classDiff <= (Math.min(...homeworkDifficulty))) { //homework pushed to the last if less difficult
       priorList.push(homeworks[i].name);
     }
   }
   console.log(priorList);
 
-  //shows the priority onto the web page
+  /*shows the priority onto the web page*/
   document.querySelector(".hw-manager").appendChild(document.createElement("h3")).innerText = "Here's how you should do your homework in order";
+
   for(x = 0; x < numOfHw; x++){
     var priorHomework = document.createElement("ul");
     var priorHomeworkName = document.createTextNode((x+1) + ") " + priorList[x]);
