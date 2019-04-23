e8c074c1fdeff3c7d2c0144eb86162c8a95a0d34
TheAmazingBeat <38993027+TheAmazingBeat@users.noreply.github.com>
2019-04-23 11:39:17 -0500

    Updated Homework Menu

--- script_v3.js
+++ script_v3.js
@@ -64,13 +64,20 @@ function next(){
 }
 
 
+function deleteHomework(){
+    document.querySelector(".homework-item").parentElement.removeChild(document.querySelector(".homework-item"));
+}
+
+
 /*Adds a text input, dropdown menus for type, class, and due date*/
 function addHomework() {
     var homeworkItem = document.createElement("li");
+    homeworkItem.classList.add("homework-item");
 
     var hwInput = document.createElement("input");
     hwInput.type = "text";
     hwInput.classList.add("homework-input");
+    hwInput.placeholder = "Name of Homework";
 
     /*creates a dropdown menu for homework type, then the options in it*/
     var hwType = document.createElement("select");
@@ -88,8 +95,12 @@ function addHomework() {
     var hwDate = document.createElement("input");
     hwDate.classList.add("hw-date");
     hwDate.setAttribute("type", "date");
-    hwDate.setAttribute("value", year + "-" + "0" + month + "-" + day);
-
+    if(day < 10){
+        hwDate.setAttribute("value", year + "-" + "0" + month + "-" + "0" + day);
+    } else{
+        hwDate.setAttribute("value", year + "-" + "0" + month + "-" + day);
+    }
+    
     /* creates a dropdown menu for what class the homework is for */
     var hwClass = document.createElement("select");
     hwClass.classList.add("hw-class");
@@ -126,28 +137,6 @@ function storeHW(numOfHw) {
 /*organizes the homework from highest difficulty and lowest difficulty*/
 function prioritize(numOfHw) {
     storeHW(numOfHw);
-
-    for (i = 0; i < numOfHw; i++) {
-        if (homeworks[i].type === 'Major') { //if it is a major homework, it will be the very first
-            priorList.unshift(homeworks[i].name);
-        } else if (homeworks[i].classDiff >= (Math.max(...homeworks[i].difficulty))) {
-            if (priorList.indexOf(priorList[i - 1]) == 0 && homeworks[i - 1].type != 'Major') { //replaces the one in front, if and only the front is not a major homework
-                priorList.splice(0, 0, homeworks[i].name);
-            } else {
-                priorList.splice(1, 0, homeworks[i].name);
-            }
-        } else if (homeworks[i].classDiff >= 8) { //8 and 9 difficulty are near the first
-            priorList.splice(priorList.length - 4, 0, homeworks[i].name);
-        } else if (homeworks[i].classDiff >= 6) { //6 and 7 difficulty are near the first
-            priorList.splice(priorList.length - 3, 0, homeworks[i].name);
-        } else if (homeworks[i].classDiff >= 4) { //4 and 5 difficulty are in the middle
-            priorList.splice(priorList.length - 2, 0, homeworks[i].name);
-        } else if (homeworks[i].classDiff >= 2) { //2 and 3 difficulty are near the last homework
-            priorList.splice(priorList.length - 1, 0, homeworks[i].name);
-        } else if (homeworks[i].classDiff <= (Math.min(...homeworkDifficulty))) { //homework pushed to the last if less difficult
-            priorList.push(homeworks[i].name);
-        }
-    }
     console.log(priorList);
 
     /*shows the priority onto the web page*/
